50f02c2794c81af29bb83046983363398adf69b6
balagan <balagan73@gmail.com>
2016-11-20 19:17:25 +0100

    Displays images with alt, title, and size data underneath.

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 var apikey = '06978083e5080079f06986a732851025';
-var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search';
-url += '&' + 'api_key=' + apikey + '&tags=truffle&format=json&nojsoncallback=1';
+var searchUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.search';
+searchUrl += '&' + 'api_key=' + apikey + '&tags=truffle&format=json&nojsoncallback=1';
 var app = angular.module('flickrApp', []);
 var output = null;
 
 app.controller('flickrCtrl', function($scope, $http) {
-    $http.get(url)
-    .then(function(response) {
-        $scope.output = response.data;
+    $http.get(searchUrl)
+    .then(function(searchResponse) {
+        $scope.output = searchResponse.data;
         angular.forEach($scope.output.photos.photo, function (value, key) {
         	value.url = 'https://farm' + value.farm + '.staticflickr.com/' + value.server + '/' + value.id + '_' + value.secret + '.jpg';
-        	$scope.output.photos.photo[key] = value;
+        	var sizeUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.getSizes';
+        	sizeUrl += '&' + 'api_key=' + apikey + '&photo_id=' + value.id + '&format=json&nojsoncallback=1';
+        	$http.get(sizeUrl)
+        	.then(function(sizeResponse) {
+        		value.sizes = sizeResponse.data.sizes.size;
+        		$scope.output.photos.photo[key] = value;
+        	})
         });
     });
 });
