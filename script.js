e8dcad64162f311df72a64bde179124145fad5f8
balagan <balagan73@gmail.com>
2016-11-20 17:43:37 +0100

    Constructs the url of the image and adds it to the objects returned.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var apikey = 'bdfeb595210d01c6ea35d53a44557f8e';
+var apikey = '06978083e5080079f06986a732851025';
 var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search';
 url += '&' + 'api_key=' + apikey + '&tags=truffle&format=json&nojsoncallback=1';
 var app = angular.module('flickrApp', []);
@@ -8,5 +8,9 @@ app.controller('flickrCtrl', function($scope, $http) {
     $http.get(url)
     .then(function(response) {
         $scope.output = response.data;
+        angular.forEach($scope.output.photos.photo, function (value, key) {
+        	value.url = 'https://farm' + value.farm + '.staticflickr.com/' + value.server + '/' + value.id + '_' + value.secret + '.jpg';
+        	$scope.output.photos.photo[key] = value;
+        });
     });
 });
