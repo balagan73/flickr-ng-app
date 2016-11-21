2a4208d29e7b7e058f4695df1cd495beb88f4749
balagan <balagan73@gmail.com>
2016-11-21 17:42:23 +0100

    Add search field to the top of the page.

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 var app = angular.module('flickrApp', []);
 app.controller('flickrCtrl', function($scope, $http) {
+
 	var apikey = '57f694132e4714c29a64c9af890b124e';
-	var searchUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.search';
-	searchUrl += '&' + 'api_key=' + apikey + '&tags=colors&format=json&nojsoncallback=1';
+    var initTerm = '&user_id=peziza';
 	var output = null;
     var hovered = false;
-    $http.get(searchUrl)
-    .then(function(searchResponse) {
-        $scope.output = searchResponse.data;
-        angular.forEach($scope.output.photos.photo, function (photoItem, key) {
-        	//photoItem.url = 'https://farm' + photoItem.farm + '.staticflickr.com/' + photoItem.server + '/' + photoItem.id + '_' + photoItem.secret + '.jpg';
-        	var sizeUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.getSizes';
-        	sizeUrl += '&' + 'api_key=' + apikey + '&photo_id=' + photoItem.id + '&format=json&nojsoncallback=1';
-        	$http.get(sizeUrl)
-        	.then(function(sizeResponse) {
-        		photoItem.sizes = sizeResponse.data.sizes.size;
-                photoItem.url = sizeResponse.data.sizes.size[1].source;
-        		$scope.output.photos.photo[key] = photoItem;
-        	})
+
+    $scope.submit = function(searchTerm) {
+        console.log(searchTerm);
+        var searchUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.search';
+        searchUrl += '&' + 'api_key=' + apikey + searchTerm + '&format=json&nojsoncallback=1';
+        $http.get(searchUrl)
+        .then(function(searchResponse) {
+            $scope.output = searchResponse.data;
+            angular.forEach($scope.output.photos.photo, function (photoItem, key) {
+                //photoItem.url = 'https://farm' + photoItem.farm + '.staticflickr.com/' + photoItem.server + '/' + photoItem.id + '_' + photoItem.secret + '.jpg';
+                var sizeUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.getSizes';
+                sizeUrl += '&' + 'api_key=' + apikey + '&photo_id=' + photoItem.id + '&format=json&nojsoncallback=1';
+                $http.get(sizeUrl)
+                .then(function(sizeResponse) {
+                    photoItem.sizes = sizeResponse.data.sizes.size;
+                    photoItem.url = sizeResponse.data.sizes.size[1].source;
+                    $scope.output.photos.photo[key] = photoItem;
+                })
+            });
+
         });
-    });
+    }
+
+    $scope.submit(initTerm);
+
 });
